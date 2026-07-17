a109dcf721ccd7ed0651d06fc2cde22cdef655a8
Ivan Flores <201900444@est.umss.edu>
2026-07-17 08:45:34 -0300

    Actualizando archivo rss.xml

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,30 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection(`${collections}`);
+	const cuentos = await getCollection('cuento');
+	const poemas = await getCollection('poema');
+	const escritosCuentos = cuentos.map((post) => ({
+		...post.data,
+		link: `/cuento/${post.slug}/`,
+	}));
+
+	const escritosPoemas = poemas.map((post) => ({
+		...post.data,
+		link: `/poema/${post.slug}/`,
+	}));
+
+	const todosLosEscritos = [...escritosCuentos, ...escritosPoemas].sort(
+		(a, b) => b.pubDate.valueOf() - a.pubDate.valueOf()
+	);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/${collections}/${post.id}/`,
+		items: todosLosEscritos.map((escrito) => ({
+			title: escrito.title,
+			pubDate: escrito.pubDate,
+			description: escrito.description,
+			link: escrito.link, // Usa el link que armamos arriba dinámicamente
 		})),
 	});
 }
